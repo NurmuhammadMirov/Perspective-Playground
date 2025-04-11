0b312683623b761ca11ba996550913fc00733cb5
Mirov <mirov0727@gmail.com>
2025-04-12 02:16:55 +0800

    add color picker

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ Vue.createApp({
       perspective: 100,
       rotateX: 0,
       rotateY: 0,
-      rotateZ: 0
+      rotateZ: 0,
+      color: '#8D81F3'
     }
   }, 
   computed: {
@@ -15,22 +16,27 @@ Vue.createApp({
           rotateX(${this.rotateX}deg)
           rotateY(${this.rotateY}deg)
           rotateZ(${this.rotateZ}deg)
-        `
+        `,
+        background: this.color
       }
     }
   },
   methods: {
     reset() {
-      this.perspective = 100
-      this.rotateX = 0
-      this.rotateY = 0
-      this.rotateZ = 0
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+      this.color = '#8D81F3';
     },
     async copy() {
-      let text = `transform:${this.box.transform};`
+      let text = `transform:${this.box.transform};background: "${this.box.background}";`
       await navigator.clipboard.writeText(text);
 
       alert("CSS Copied to Clipboard!");
+    },
+    colorChange(e) {
+      this.color = e.target.value;
     }
   }
 }).mount('#app');
